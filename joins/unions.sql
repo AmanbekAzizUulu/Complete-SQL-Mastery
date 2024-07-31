f9d90d9d0a12f2c6af51a1128828eb252200a73c
Amanbek Aziz Uulu <amanbekazizuulu@gmail.com>
2024-07-31 21:53:35 +0600

    example of using 'insert into' statement was added

--- joins/unions.sql
+++ joins/unions.sql
@@ -1,6 +1,102 @@
+--  Unions
+
 use sql_store;
 
+/*
+-- union operator using with 1 table selection
+
 select
-	*
+	order_id,
+    order_date,
+    'active' as status
 from 
-	orders as o;
+	orders as o
+where 
+	order_date >= '2019-01-01'
+union
+
+select
+	order_id,
+    order_date,
+    'archived' as status
+from
+	orders as o
+where 
+	order_date < '2019-01-01';
+    
+*/
+
+/*
+
+
+-- union operator using with 2 different table selections
+-- generating 2 columns 
+
+select
+	first_name,
+    '-' as shipper_name
+from
+	customers as c
+union 
+
+select
+	'-' as first_name,
+	shipper_name
+from
+	shippers as sh;
+ 
+ 
+-- generating 1 column 
+
+select
+	first_name as full_name
+from
+	customers
+union
+
+select
+	shipper_name
+from
+	shippers;
+
+ */
+ 
+-- UNION combines the results of these queries, excluding duplicate rows.
+-- UNION ALL combines the results of these queries, including all duplicate rows.
+ 
+ 
+-- exercise
+ 
+select 
+	customer_id,
+    first_name,
+    points,
+	'bronze' as type_of_account
+from
+	customers
+where
+	points < 2000
+union all
+
+select 
+	customer_id,
+    first_name,
+    points,
+    'silver' as type_of_account
+from 
+	customers
+where 
+	points between 2000 and 3000
+union all
+
+select
+	customer_id,
+    first_name,
+    points,
+    'gold' as type_of_account
+from
+	customers
+where 
+	points > 3000
+order by
+	first_name;
---
+++ statements/insert_into__statement.sql
@@ -0,0 +1,50 @@
+use sql_store;
+
+/*
+insert into
+	customers
+values(
+		default,
+        'Aibek',
+        'Dandaev',
+        '1998-10-02',
+        null,
+        'Togolok Moldo street, 12/4',
+        'Bishkek',
+        'CA',
+        2403
+    );
+    
+*/
+
+insert into
+	customers (
+			first_name,
+			last_name,
+			birth_date,
+			phone_number,
+			address,
+			city,
+			state,
+			points
+    )
+values (
+		'Soffie',
+        'Petrova',
+        '1994-01-31',
+        null,
+        'New York',
+        'NY',
+        1324
+);
+	
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
