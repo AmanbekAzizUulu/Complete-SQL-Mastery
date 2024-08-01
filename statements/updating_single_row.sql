d3026ebc278c5bd269e9bd1fe3fcba75513d94a2
Amanbek Aziz Uulu <amanbekazizuulu@gmail.com>
2024-08-01 06:01:06 +0600

    example of updating a single row using 'update ___ set' statements

---
+++ statements/updating_single_row.sql
@@ -0,0 +1,43 @@
+-- Updating a Single Row
+
+use sql_invoicing;
+
+/*
+update 
+	invoices
+set 
+	payment_total = 10,
+    payment_date = '2019-03-01'
+where
+	invoice_id = 1;
+*/
+
+/*
+update 
+	invoices 
+set
+	payment_total = 0,
+    payment_date = null
+where 
+	invoice_id = 1;
+*/
+
+/*
+update 
+	invoices
+set
+	payment_total = default,
+    payment_date = default
+where 
+	invoice_id = 1;
+*/
+
+/*
+update 
+	invoices
+set
+	payment_total = invoice_total * 0.5,
+    payment_date = due_date
+where
+	invoice_id = 3;
+*/
