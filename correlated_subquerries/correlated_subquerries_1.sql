57e947028f5afe91a30f1c5f0202643b1ca85ad9
Amanbek <120076412+AmanbekAzizUulu@users.noreply.github.com>
2024-08-08 04:48:54 +0600

    update correlated_subquerries_1.sql - added explanational comments

--- correlated_subquerries/correlated_subquerries_1.sql
+++ correlated_subquerries/correlated_subquerries_1.sql
@@ -42,18 +42,20 @@ where exists
 -- exercise
 -- find the products that have never been ordered 
 
-use sql_store;
 
-select
-	*
-from
-	products as p
-where not exists 
-	(
-		select
-			distinct product_id
-		from 
-			order_items as oi
-		where 
-			product_id = p.product_id
-	)
+use sql_store; 					-- Переключаемся на базу данных sql_store
+
+SELECT						-- Выбираем все столбцы из таблицы products
+    *
+FROM
+    products AS p
+WHERE EXISTS 					-- Фильтруем строки, возвращая только те, для которых подзапрос внутри EXISTS возвращает хотя бы одну строку
+    (
+        SELECT					-- Подзапрос: выбираем уникальные значения product_id из таблицы order_items
+            DISTINCT product_id
+        FROM 
+            order_items AS oi
+       
+        WHERE 					-- Условие связывает подзапрос с внешним запросом, проверяя, 
+	    product_id = p.product_id		-- что product_id в подзапросе равен product_id из текущей строки внешнего запроса      
+    );
