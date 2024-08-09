358efd0fef72cf7ffe583693a4212105627709e3
Amanbek <120076412+AmanbekAzizUulu@users.noreply.github.com>
2024-08-10 04:35:38 +0600

    update calculating_dates_and_times.sql

--- built_in_functions/calculating_dates_and_times.sql
+++ built_in_functions/calculating_dates_and_times.sql
@@ -5,8 +5,8 @@ select
 	date_add(now(), interval 1 year) as 'current_year + 1',
     datediff('2024-01-01', now()),
 	datediff( now(), '2024-01-01'),
-    time_to_sec(now()) - time_to_sec('04:26');	-- TIME_TO_SEC(time)
-												-- Returns the time argument, converted to seconds. Returns NULL if time is NULL.
+    time_to_sec(now()) - time_to_sec('04:26');		-- TIME_TO_SEC(time)
+							-- Returns the time argument, converted to seconds. Returns NULL if time is NULL.
     
 -- DATE_ADD() используется для добавления интервала к дате или времени.
 -- DATE_SUB() используется для вычитания интервала из даты или времени.
