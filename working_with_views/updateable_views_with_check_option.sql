4656ddd73b06edad3bb6d563021ead5e69d8976a
Amanbek <amanbekazizuulu@gmail.com>
2024-08-12 02:26:32 +0600

    updated 'updateable_views_with_check_option.sql'

--- working_with_views/updateable_views_with_check_option.sql
+++ working_with_views/updateable_views_with_check_option.sql
@@ -21,3 +21,22 @@ view invoices_with_balance_with_check_option as
 	with check option;
     
     
+-- in this view, the WITH CHECK OPTION ensures that any updates or inserts through 
+-- this view will only allow invoices where the balance is greater than 0 
+-- (i.e., where (invoice_total - payment_total) > 0)
+
+-- so if you try to modify a row in such a way that it would no longer be included 
+-- in the view you will get ann error:
+
+
+update
+	invoices_with_balance_with_check_option
+set
+	payment_total = invoice_total
+where
+	invoice_id = 5;
+    
+-- if this querry executes, the error below will appear:
+-- Error Code: 1369. CHECK OPTION failed 'sql_invoicing.invoices_with_balance_with_check_option'
+-- < Error Code: 1369. CHECK OPTION failed 'sql_invoicing.invoices_with_balance_with_check_option' >
+
