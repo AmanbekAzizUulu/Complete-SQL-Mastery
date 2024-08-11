3c7cd90280af6b0edea8883371189f03a8c60be2
Amanbek <120076412+AmanbekAzizUulu@users.noreply.github.com>
2024-08-12 03:13:22 +0600

    refactor get_invoices_with_balance.sql

--- stored_procedures/get_invoices_with_balance.sql
+++ stored_procedures/get_invoices_with_balance.sql
@@ -2,19 +2,19 @@ delimiter $$
 create procedure get_invoices_with_balance ()
 begin
     select 
-        invoices.invoice_id AS invoice_id,
-        invoices.invoice_number AS invoice_number,
-		invoices.client_id AS client_id,
-        invoices.invoice_total AS invoice_total,
-        invoices.payment_total AS payment_total,
-        (invoices.invoice_total - invoices.payment_total) AS balance,
-        invoices.invoice_date AS invoice_date,
-        invoices.due_date AS due_date,
-        invoices.payment_date AS payment_date
+        invoice_id AS invoice_id,
+        invoice_number AS invoice_number,
+		client_id AS client_id,
+        invoice_total AS invoice_total,
+        payment_total AS payment_total,
+        (invoice_total - payment_total) AS balance,
+        invoice_date AS invoice_date,
+        due_date AS due_date,
+        payment_date AS payment_date
     from
         invoices
     where
-        (invoices.invoice_total - invoices.payment_total) > 0 ;
+        (invoice_total - payment_total) > 0 ;
 end $$
 delimiter ;
     
